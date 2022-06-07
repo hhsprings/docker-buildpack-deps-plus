38e79fca89b8e0281df76d703c1fe8ac25d82c97
Hiroaki Itoh <xwhhsprings@gmail.com>
2022-06-08 07:20:29 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,25 +5,25 @@ target_latest=22.10
 
 trap 'rm -f _suites' 0 1 2 3 15
 cat << __EOF__ > _suites
-#bookworm
-#bullseye
-#buster
-#sid
-#stretch
-#xenial
-#16.04
-#bionic
-#18.04
-#focal
+bookworm
+bullseye
+buster
+sid
+stretch
+xenial
+16.04
+bionic
+18.04
+focal
 20.04
-#hirsute
-#21.04
-#impish
-#21.10
-#jammy
-#22.04
-#kinetic
-#22.10
+hirsute
+21.04
+impish
+21.10
+jammy
+22.04
+kinetic
+22.10
 __EOF__
 
 #docker buildx create --name=mybuilder --use  # for multiple platforms
@@ -36,8 +36,8 @@ for bpd_ver in `tac _suites | grep -v ^#` ; do
     docker buildx build -f Dockerfile \
            ${t_opt} \
            --build-arg _BUILDPACKDEPS_TAG=${bpd_ver} \
-           --build-arg _BUILDPACKDEPS_PLAT="arm64v8/" \
-           --platform linux/arm64/v8 \
+           --build-arg _BUILDPACKDEPS_PLAT="" \
+           --platform linux/amd64 \
            -o type=image,push=${__push} \
            .
 done
