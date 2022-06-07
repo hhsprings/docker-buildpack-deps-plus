8645cb5a5d3fe4e10c9db78b71b0f37130715b4d
Hiroaki Itoh <xwhhsprings@gmail.com>
2022-06-08 07:32:27 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ for bpd_ver in `tac _suites | grep -v ^#` ; do
     docker buildx build -f Dockerfile \
            ${t_opt} \
            --build-arg _BUILDPACKDEPS_TAG=${bpd_ver} \
-           --platform linux/amd64 \
+           --platform amd64 \
            -o type=image,push=${__push} \
            .
 done
